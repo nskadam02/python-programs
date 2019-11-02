4be700c822af839992aaaebff248af339ad3c038
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2019-11-02 16:17:11 +0530

    assignment2_7
    it display pattern
    1 2 3 4 5
    1 2 3 4 5
    1 2 3 4 5
     1 2 3 4 5
    1 2 3 4 5

---
+++ pattern3.py
@@ -0,0 +1,9 @@
+def Pattern():
+    num=int(input("Enter Number"));
+    for i in range(num):
+        for i in range(1,num+1):
+            print(i,end=" ");
+
+        print("\r");    
+
+Pattern();
