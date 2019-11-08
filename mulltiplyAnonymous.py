67c0834e3b4e9a01241bb3edc51f37baa3b0071a
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2019-11-08 21:23:33 +0530

    assignment4_2.py
    in this there is anonymous function which take two argument and return multiplication of it

---
+++ mulltiplyAnonymous.py
@@ -0,0 +1,5 @@
+#example of lambda funtion
+fp=lambda no1,no2:no1*no2;
+no1=int(input("Enter first number for multoiplication:"));
+no2=int(input("Enter second number for multoiplication:"));
+print("Multiplication of",no1,no2,"is:",fp(no1,no2))
